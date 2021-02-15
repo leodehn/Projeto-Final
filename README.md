24822b2b8f7d982ecdbca89badb7b35906ac2f7a
Leonardo <leo.dehn@hotmail.com>
2021-02-15 12:27:26 -0300

    Subindo-Projeto

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Projeto-Final
+Feito por Leonardo Ferreira & Nathane Ribeiro
+
+Com os conhecimentos da matéria de DSI - IFSP - Araraquara - 2020 - S2
---
+++ proj/conexao/ConectaHibernateMySQL.java
@@ -0,0 +1,16 @@
+package proj.conexao;
+
+import org.hibernate.Session;
+
+public class ConectaHibernateMySQL {
+	
+	public static void main(String[] args) {
+		Session sessao = null;
+		try {
+		sessao = HibernateUtil.getSessionFactory().openSession();
+		} finally {
+			sessao.close();
+		}
+	}
+
+}
